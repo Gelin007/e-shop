9115bf8b1d1b94cd0d25bf9b69ad4056f5b3946c
Gelin Nguedong <gelinhope@hotmail.com>
2022-06-30 02:48:37 +0200

    Some components added and routing for checkout and home implemented

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Checkout from './components/Checkout';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/checkout" element={<div><Header /><Checkout /></div>} />
+          <Route path="/" element={<div><Header /><Home /></div>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Header.css";
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SearchIcon from '@material-ui/icons/Search';
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <div className="header">
+            <Link to="/" style={{ textDecoration: "none"}}>
+                <div className="header__logo">
+                    <StorefrontIcon className="header__logoImage" fontSize="large" />
+                    <h2 className="header__logoTitle">eShop</h2>
+                </div>
+            </Link>
+
+            <div className="header__search">
+                <input type="text" className="header__searchInput" />
+                <SearchIcon className="header__searchIcon" />
+            </div>
+
+            <div className="header__nav">
+                <div className="nav__item">
+                    <span className="nav__itemLineOne">Hello Guest</span>
+                    <snap className="nav__itemLineTwo">Sign In</snap>
+                </div>
+
+                <div className="nav__item">
+                    <span className="nav__itemLineOne">Your</span>
+                    <snap className="nav__itemLineTwo">Shop</snap>
+                </div>
+
+                <Link to="/checkout" style={{ textDecoration: "none" }}>
+                    <div className="nav__itemBasket">
+                        <ShoppingBasketIcon />
+                        <snap className="nav__itemLineTwo nav__basketCount">0</snap>
+                    </div>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
