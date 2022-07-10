9f2278e2fc9b46f39cf85cd6d3b88b0c9dc18afb
Gelin Nguedong <gelinhope@hotmail.com>
2022-07-10 22:20:08 +0200

    Add to basket functionality implemented with context api

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,23 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+
+    const [state, dispath] = useStateValue();
+
+    const addToBasket = () => {
+        dispath({
+            type: "ADD_TO_BASKET",
+            item: {
+                id: id,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        });
+    }
+
     return (
         <div className="product">
             <div className="product__info">
@@ -21,7 +37,7 @@ function Product({ id, title, image, price, rating }) {
 
             <img src={image} alt="" />
 
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
