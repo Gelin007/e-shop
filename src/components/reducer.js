cee61f67e85d6556066f59d897762d7c279c429f
Gelin Nguedong <gelinhope@hotmail.com>
2022-07-19 07:02:35 +0200

    show product items and subtotal data on checkout page

--- src/components/reducer.js
+++ src/components/reducer.js
@@ -6,6 +6,10 @@ export const initialState = {
 }
 
 // Selector
+export const getBasketTotal = (basket) => {
+   return(basket?.reduce((amount, item) => item.price + amount, 0)) ;
+}
+
 const reducer = (state, action) => {
     switch (action.type) {
         case "ADD_TO_BASKET":
@@ -15,7 +19,7 @@ const reducer = (state, action) => {
             }
 
         default:
-            break;
+            return state;
     }
 }
 
